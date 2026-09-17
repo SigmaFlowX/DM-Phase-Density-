3625d5c7a5846816aae1ac38b3d71a070dcdf02b
Dmitrii <bakaevdima4@gmail.com>
2026-09-17 21:08:51 +0300

    trace evolution of 1 particular halo

--- src/api_test.py
+++ src/api_test.py
@@ -19,6 +19,43 @@ headers = {"api-key":api_key}
 
 r = get(baseUrl)
 name = "TNG50-1"
-sim  = next((sim for sim in r['simulations'] if sim['name'] == name), None)
+sim_obj  = next((sim for sim in r['simulations'] if sim['name'] == name), None)
+
+sim = get(sim_obj['url'])
+
+snapshots = get(sim['snapshots'])
+
+z0snapshot = get(snapshots[-1]['url'])
+
+subhalos = get(z0snapshot['subhalos'])
+
+
+halo = get(subhalos['results'][80]['url']) #first halo
+
+
+
+
+#mass history of this specific halo
+m = [halo['mass']]
+snaps = [halo['snap']]
+for i in range(100):
+    progenitor_url = halo['related']['sublink_progenitor']
+    if progenitor_url:
+        progenitor = get(progenitor_url)
+    else:
+        break
+
+    print(progenitor)
+    m.append(progenitor['mass'])
+    snaps.append(progenitor['snap'])
+    halo = progenitor
+
+import matplotlib.pyplot as plt
+plt.plot(snaps, m)
+plt.xlabel("snap")
+plt.ylabel("mass")
+plt.grid()
+plt.show()
+
 
 
